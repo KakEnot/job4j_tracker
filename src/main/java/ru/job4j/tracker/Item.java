0dd6c60eb038a2e19f5e2b4549b24df5bcd6643a
KakEnot <k.a.isaeva@icloud.com>
2022-10-09 16:12:15 +0400

    change class Item

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,9 +4,20 @@ public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
+    public Item() {
+
     }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {return id;}
 
     public void setId(int id) {
         this.id = id;
