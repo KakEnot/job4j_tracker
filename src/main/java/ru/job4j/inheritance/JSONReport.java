f30dd993e79242191be7f0083ee7a27715f87d3a
KakEnot <k.a.isaeva@icloud.com>
2022-10-11 22:27:26 +0400

    add @Override to classes HtmlReport and JSONReport

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,9 +1,8 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-
+    @Override
     public String generate(String name, String body) {
-
         String ln = System.lineSeparator();
         return String.format("{" + ln
                 + "\t\"name\" : \"" + name + "\"," + ln
