eda382412dc6fa9628ec73f5c0c00c75204b2b2c
KakEnot <k.a.isaeva@icloud.com>
2022-10-11 22:56:01 +0400

    add StartUi

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
 
@@ -32,4 +35,17 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 }
+
+// Сразу проинициализируйте это поле текущей датой и временем,
+// используйте статический метод now().
+// Добавьте геттер, чтобы получить доступ к созданному полю.
+// Добавьте класс StartUI, в нем создайте один объект Item.
+// На этом объекте вызовите геттер поля created, выведите полученную
+// дату в консоль в отформатированном варианте, в качестве паттерна
+// используйте следующую строку:
+// "dd-MMMM-EEEE-yyyy HH:mm:ss"
