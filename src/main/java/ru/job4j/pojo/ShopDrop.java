a393370811e99ceeaedb9420c7780024e4355a04
KakEnot <k.a.isaeva@icloud.com>
2022-10-16 13:40:52 +0400

    change class ShopDrop

---
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        products[index] = null;//index=3;
+        Product temp;
+        for (int i = index; i < products.length - 1; i++) {
+            temp = products[i + 1];
+            products[i + 1] = products[i];
+            products[i] = temp;
+        }
+
+        return products;
+    }
+}
