57345020c5e01e0428eb1a9566f139eb29da33fa
KakEnot <k.a.isaeva@icloud.com>
2022-11-05 00:55:29 +0400

    change class Matches

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -13,11 +13,12 @@ public class Matches {
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
             turn = !turn;
-            while (matches < 1 || matches > 3 || matches > count) {
-                System.out.println("Ошибка! Введите число от 1 до 3:");
-                matches = Integer.parseInt(input.nextLine());
+            if (matches < 1 || matches > 3 || matches > count) {
+                System.out.println("Ошибка!");
+                turn = !turn;
+            } else {
+                count -= matches;
             }
-            count -= matches;
             System.out.println(count);
         }
         if (!turn) {
