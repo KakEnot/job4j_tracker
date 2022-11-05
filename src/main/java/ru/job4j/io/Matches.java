72a4091e7e1e45ab256da9f606adad550fe00399
KakEnot <k.a.isaeva@icloud.com>
2022-11-05 22:23:38 +0400

    change class Matches

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,12 +12,11 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-            turn = !turn;
             if (matches < 1 || matches > 3 || matches > count) {
                 System.out.println("Ошибка!");
-                turn = !turn;
             } else {
                 count -= matches;
+                turn = !turn;
             }
             System.out.println(count);
         }
