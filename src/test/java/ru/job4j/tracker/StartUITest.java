24f582e164c64df0f26d2b97f9ea17604faf4430
KakEnot <k.a.isaeva@icloud.com>
2022-11-30 11:02:35 +0400

    lesson 5.1

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -35,10 +35,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new Item");
         tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
-        };
+        String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         assertThat(tracker.findById(item.getId())).isNull();
     }
