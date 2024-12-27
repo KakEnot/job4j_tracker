ed5da467f35412c40746d7b699ea738c35661950
k.isaev1 <k.isaev@paysenger.com>
2024-12-27 11:42:51 +0100

    4. Телефонный справочник на базе ArrayList [#41598 #526670]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -12,16 +12,7 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getPhone().contains(key)) {
-                result.add(person);
-            }
-            if (person.getName().contains(key)) {
-                result.add(person);
-            }
-            if (person.getAddress().contains(key)) {
-                result.add(person);
-            }
-            if (person.getSurname().contains(key)) {
+            if (person.getPhone().contains(key) || person.getName().contains(key) || person.getAddress().contains(key) || person.getSurname().contains(key)) {
                 result.add(person);
             }
         }
