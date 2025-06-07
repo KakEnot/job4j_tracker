39f712932ab6b7413a06b258e653f38b0653a8f4
KakEnot <k.a.isaeva@icloud.com>
2025-06-07 21:14:36 +0300

    [#34352 #545449]

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -1,16 +1,15 @@
 package ru.job4j.collection;
 
 import java.util.Comparator;
-import java.util.List;
 
 public class DepartmentsDescComparator implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String tempLeft = List.of(left.split("/")).get(0);
-        String tempRight = List.of(right.split("/")).get(0);
-        if (!tempLeft.equals(tempRight)) {
-            return tempRight.compareTo(tempLeft);
+        String tempLeft = left.split("/")[0];
+        String tempRight = right.split("/")[0];
+        if (tempLeft.compareTo(tempRight) == 0) {
+            return left.compareTo(right);
         }
-        return left.compareTo(right);
+        return right.compareTo(left);
     }
 }
