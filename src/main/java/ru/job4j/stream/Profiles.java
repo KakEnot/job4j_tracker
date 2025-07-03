912d4dc8a1f0d2915159c634b43ab1a2e5e24b8d
unknown <k.a.isaeva@icloud.com>
2025-07-03 22:31:11 +0200

    [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,7 +6,17 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted((a1, a2) -> a1.getCity().compareTo(a2.getCity()))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
 
